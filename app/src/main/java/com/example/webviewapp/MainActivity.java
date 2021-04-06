28aed1799c86cb548a3ff0622733ff3ef2e62880
c20adael <c20adael@student.his.se>
2021-04-06 12:13:01 +0200

    har nu lagt till så att båda metoderna har kod som gör att webbsidor visas. Har även anpassat webView så att hela sidan visas. Har även implementerat så att if satsen kallar på metoderna.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -21,11 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
-        myWebView.loadUrl("http://www.his.se");
+        WebView myWebView = findViewById(R.id.my_webview);
+        myWebView.loadUrl("https://www.his.se");
     }
 
     public void showInternalWebPage(){
         // TODO: Add your code for showing internal web page here
+        WebView myWebView = findViewById(R.id.my_webview);
         myWebView.loadUrl("file:///android_asset/webPages/test.html");
     }
 
@@ -95,11 +97,13 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
+            showExternalWebPage();
             Log.d("==>","Will display external web page");
             return true;
         }
 
         if (id == R.id.action_internal_web) {
+            showInternalWebPage();
             Log.d("==>","Will display internal web page");
             return true;
         }
